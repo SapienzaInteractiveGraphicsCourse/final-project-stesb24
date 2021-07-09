6e9b2cd7296130f58ec637f54cf961e23d669090
Stefano Bonetti <82468205+stesb24@users.noreply.github.com>
2021-07-09 16:42:50 +0200

    Added libraries

--- character.js
+++ character.js
@@ -1,4 +1,4 @@
-import * as THREE from "./libs/threejs/build/three.module.js";
+import * as THREE from "./libs/three.module.js";
 
 //Create a new box
 function createCharacter(boxWidth, boxHeight, boxNumber) {
--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import * as THREE from "./libs/threejs/build/three.module.js";
+import * as THREE from "./libs/three.module.js";    //r130
 import {createCharacter} from "./character.js";
 import {resizeRendererToDisplaySize} from "./utils.js";
 
