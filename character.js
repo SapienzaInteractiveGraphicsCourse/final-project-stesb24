6085b187faebf08024cd159495f5df2882e054e8
Stefano Bonetti <82468205+stesb24@users.noreply.github.com>
2021-07-15 17:03:19 +0200

    Better listener, more boxes

--- character.js
+++ character.js
@@ -1,9 +1,9 @@
 import * as THREE from "./libs/three.module.js";
 import {makeCamera} from "./game.js";
 
-//i-th value is the initial x coordinate of the i-th box
-const initialPositions = [
-    -10, 10
+//i-th value is the initial (x, z) position of the i-th box
+const initialCoordinates = [
+    [-14, -20], [15, -10], [13, 10], [-5, 0]
 ];
 
 //Create a new box and return it with its camera
@@ -21,9 +21,10 @@ function createCharacter(boxWidth, boxHeight, boxNumber) {
         boxMaterial.color.set("blue");
     }
     
-    const initialX = initialPositions[boxNumber];
+    const initialX = initialCoordinates[boxNumber][0];
     const initialY = boxHeight / 2;
-    boxMesh.position.set(initialX, initialY, 0);
+    const initialZ = initialCoordinates[boxNumber][1];
+    boxMesh.position.set(initialX, initialY, initialZ);
 
     //Add third person and first person cameras to the box
     const thirdPersonCamera = makeCamera();
@@ -32,7 +33,7 @@ function createCharacter(boxWidth, boxHeight, boxNumber) {
     boxMesh.add(thirdPersonCamera);
 
     const firstPersonCamera = makeCamera();
-    firstPersonCamera.position.set(0, boxHeight * 0.25, 0);       //Relative to the box
+    firstPersonCamera.position.set(0, boxHeight * 0.25, 0);         //Relative to the box
     firstPersonCamera.lookAt(0, boxHeight * 0.25, -1);
     boxMesh.add(firstPersonCamera);
 
@@ -41,7 +42,7 @@ function createCharacter(boxWidth, boxHeight, boxNumber) {
     const boxShape = new CANNON.Box(halfExtents);
     const boxBody = new CANNON.Body({mass: 0});
     boxBody.addShape(boxShape);
-    boxBody.position.set(initialX, initialY, 0);
+    boxBody.position.set(initialX, initialY, initialZ);
 
     return [boxMesh, boxBody, thirdPersonCamera, firstPersonCamera];
 }
--- game.js
+++ game.js
@@ -16,8 +16,10 @@ let globalCamera;
 const bullets = [];
 const bulletBodies = [];
 
-const players = 2;
-let currentPlayer = 0;
+const numTeams = 2;
+const boxesPerTeam = 2;
+const numBoxes = numTeams * boxesPerTeam;
+let currentBoxNumber = 0;
 
 //Creates new cameras
 function makeCamera(near = 1, far = 80) {
@@ -41,7 +43,7 @@ function main() {
     createMap(scene, world);
 
     //Create the boxes and their cameras
-    for (let i=0; i < players; i++) {
+    for (let i=0; i < numBoxes; i++) {
         const [boxMesh, boxBody, thirdPersonCamera, firstPersonCamera] = createCharacter(boxWidth, boxHeight, i);
 
         boxes.push(boxMesh);
@@ -58,71 +60,140 @@ function main() {
     globalCamera.position.set(0, 60, 0);
     globalCamera.lookAt(0, 0, 0);
 
-    let camera = thirdPersonCameras[0];        //Start from first player's camera
+    let camera = thirdPersonCameras[0];        //Start from first character's camera
 
     //Keyboard controls
-    let waitForCollision = false;
-    document.addEventListener("keydown", commands);     //Normal keyboard handler (disabled after shooting)
-    document.addEventListener("keydown", (e) => {       //You can always look from above
-        if (e.code == "KeyE") {
-            camera = globalCamera;
-        }
-    });
-    document.addEventListener("keyup", (e) => {         //Stop looking from above or from first person
-        if (e.code == "KeyE" || e.code == "KeyQ") {
-            camera = thirdPersonCameras[currentPlayer];
-        }
-    });
-    
-    function commands(e) {
-        if (!waitForCollision) {        //Move if you haven't shot yet; otherwise wait for the bullet to collide
-            const currentBox = boxes[currentPlayer];
-            const currentBoxBody = boxBodies[currentPlayer];
-            const boxSpeed = 0.4;
-            //How much the box moves on x and z and how much it rotates
-            const movementX = Math.sin(currentBox.rotation.y) * boxSpeed;
-            const movementZ = Math.cos(currentBox.rotation.y) * boxSpeed;
-            const boxRotation = 0.05;
-            
-            switch (e.code) {           //Remember to update the physics body accordingly
-                case "KeyW":            //Move forward        
-                    currentBox.position.x += -movementX;
-                    currentBox.position.z += -movementZ;
-                    currentBoxBody.position.x += -movementX;
-                    currentBoxBody.position.z += -movementZ;
-                    break;
-                case "KeyS":            //Move backwards
-                    currentBox.position.x += movementX;
-                    currentBox.position.z += movementZ;
-                    currentBoxBody.position.x += movementX;
-                    currentBoxBody.position.z += movementZ;
-                    break;
-                case "KeyA":            //Rotate left
-                    currentBox.rotation.y += boxRotation;
-                    currentBoxBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), currentBox.rotation.y);
-                    break;
-                case "KeyD":            //Rotate right
-                    currentBox.rotation.y += -boxRotation;
-                    currentBoxBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), currentBox.rotation.y);
-                    break;
-                case "KeyQ":            //First person
-                    camera = firstPersonCameras[currentPlayer];
-                    break;
-                case "Space":           //Shoot and go to next player's turn
-                    waitForCollision = true;            //Can't move before the collision
+    let moveForward = false;
+    let moveBackward = false;
+    let turnLeft = false;
+    let turnRight = false;
+    let global = false;                 //True = looking from above
+    let firstPerson = false;            //True = first person camera
+    let waitForCollision = false;       //True = shot fired -> don't act and wait for next turn
+
+    document.addEventListener("keydown", (e) => {
+        switch (e.code) {
+            //Move or shoot only if you haven't shot yet
+            case "KeyW":                //Move forward
+                if (!waitForCollision) {
+                    moveForward = true;
+                }
+                break;
+            case "KeyS":                //Move backward
+                if (!waitForCollision) {
+                    moveBackward = true;
+                }
+                break;
+            case "KeyA":                //Turn left
+                if (!waitForCollision) {
+                    turnLeft = true;
+                }
+                break;
+            case "KeyD":                //Turn right
+                if (!waitForCollision) {
+                    turnRight = true;
+                }
+                break;
+            case "Space":               //Shoot
+                if (!waitForCollision) {
+                    waitForCollision = true;        //Stop acting
+                    moveForward = false;
+                    moveBackward = false;
+                    turnLeft = false;
+                    turnRight = false;
                     const bulletBody = bullet();
-                    bulletBody.addEventListener("collide", nextTurn);       //Detect bullet collision
-                    break;
-            }
+                    bulletBody.addEventListener("collide", nextTurn);
+                }
+                break;
+            //Camera handling
+            case "KeyE":                //Global camera
+                if (!global) {          //Switch to global camera
+                    global = true;
+                    firstPerson = false;
+                    camera = globalCamera;
+                }
+                else {                  //Switch back to third person camera
+                    global = false;
+                    firstPerson = false;
+                    camera = thirdPersonCameras[currentBoxNumber];
+                }
+                break;
+            case "KeyQ":                //First person camera
+                if (!firstPerson) {     //Switch to first person camera
+                    firstPerson = true;
+                    global = false;
+                    camera = firstPersonCameras[currentBoxNumber];
+                }
+                else {                  //Switch back to third person camera
+                    firstPerson = false;
+                    global = false;
+                    camera = thirdPersonCameras[currentBoxNumber];
+                }
+                break;
+            
         }
-    }
+    });
+    document.addEventListener("keyup", (e) => {
+        //Stop moving
+        switch (e.code) {
+            case "KeyW":
+                moveForward = false;
+                break;
+            case "KeyS":
+                moveBackward = false;
+                break;
+            case "KeyA":
+                turnLeft = false;
+                break;
+            case "KeyD":
+                turnRight = false;
+                break;
+        }
+    });
 
     //Go to next player's turn
     function nextTurn(e) {
         this.removeEventListener("collide", nextTurn);  //Remove listener from bullet (detect only one collision)
-        currentPlayer = (currentPlayer + 1) % players;
-        camera = thirdPersonCameras[currentPlayer];                //Switch to next player's camera
-        waitForCollision = false;
+        setTimeout(() => {                              //Change turn some time after the collision
+            currentBoxNumber = (currentBoxNumber + 1) % numBoxes;
+            camera = thirdPersonCameras[currentBoxNumber];       //Switch to next player's camera
+            global = false;                             //Reset everything
+            firstPerson = false;
+            waitForCollision = false;
+        }, 1500);
+    }
+
+    //Move the box and copy the coordinates to its physics body (called at every render)
+    function move() {
+        const currentBox = boxes[currentBoxNumber];
+        const currentBoxBody = boxBodies[currentBoxNumber];
+        const boxSpeed = 0.1;
+        //How much the box moves on x and z and how much it rotates
+        const movementX = Math.sin(currentBox.rotation.y) * boxSpeed;
+        const movementZ = Math.cos(currentBox.rotation.y) * boxSpeed;
+        const boxRotation = 0.015;
+
+        //No if-else so that you can use them together
+        if (moveForward) {
+            currentBox.position.x += -movementX;
+            currentBox.position.z += -movementZ;
+            currentBoxBody.position.x += -movementX;
+            currentBoxBody.position.z += -movementZ;
+        }
+        if (moveBackward) {
+            currentBox.position.x += movementX;
+            currentBox.position.z += movementZ;
+            currentBoxBody.position.x += movementX;
+            currentBoxBody.position.z += movementZ;
+        }
+        if (turnLeft) {
+            currentBox.rotation.y += boxRotation;
+            currentBoxBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), currentBox.rotation.y);
+        }
+        if (turnRight) {
+            currentBox.rotation.y += -boxRotation;
+            currentBoxBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), currentBox.rotation.y);
+        }
     }
 
     //Shoot a new bullet
@@ -135,7 +206,7 @@ function main() {
         const bulletMesh = new THREE.Mesh(bulletGeometry, bulletMaterial);
 
         //Bullet initial position
-        const currentBox = boxes[currentPlayer];
+        const currentBox = boxes[currentBoxNumber];
         const angle = currentBox.rotation.y;
         const initialX = currentBox.position.x - Math.sin(angle) * 0.75;    //Bullet spawns a bit distant from the box
         const initialY = boxHeight/2;
@@ -168,6 +239,9 @@ function main() {
     function render() {
         //Step the physics world
         world.step(1/60);
+
+        //Move the box
+        move();
 
         //Copy coordinates from CANNON to THREE for each bullet
         bullets.forEach((bullet, index) => {
